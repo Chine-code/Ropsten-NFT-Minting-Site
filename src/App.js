a2c2a102609a71a0702a5780aaa6e69cd4236bcc
Chine-Code <chokejiri@gmail.com>
2022-02-17 01:11:59 +0100

    Added Mint Button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() 
+{
+ return (
+  <div className="App">
+    <p>Get Ready to mint something cool</p>
+   <h1>Mint my NFT</h1>
+   <button>Mint❤️‍🔥❤️‍🔥</button>
+  </div >
+ );
 }
 
 export default App;
