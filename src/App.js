089065025e08ce5e84088b5b3fe4a7706602509e
Chine-Code <chokejiri@gmail.com>
2022-02-20 20:28:48 +0100

    completed frontend and minted nft

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //import { getAccount } from "simple-web3";
 import {ethers} from "ethers";
 //import Artifact from "../artifacts/contracts/GoldenMan.sol/GoldenMan.json";
@@ -10,43 +10,64 @@ import { CONTRACT_ABI, CONTRACT_ADDRESS, GAS_LIMIT } from "./config";
 function App() 
 {
 
-  const [loading, setLoading] = useState(false)
-  const mintsAnNFT = async () => {
-
-  try {
-    setLoading(true)
-//     let ACCOUNT = await getAccount()
-// Connect web3
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-const accounts = await provider.listAccounts();
-console.log(accounts[0]);
-const ACCOUNT = accounts[0];
-
-const signer = provider.getSigner();
-
-
-const contract = new ethers.Contract(
-    CONTRACT_ADDRESS,
-    CONTRACT_ABI,
-    signer
-);
-
-
-    await contract.mint().send({
-      from: ACCOUNT,
-      gasLimit: GAS_LIMIT,
-      value: 0
-    })
-
-      setLoading(false)
-      alert('Yayy! minted a GoldenMan')
-    } catch (e) {
-      setLoading(false)
-      console.log(e)
+  const [loading, setLoading] = useState(false);
+  const [account, setAccount] = useState([]);
+  
+  async function connectAccounts() {
+      if (window.ethereum)
+      {
+        const accounts = await window.ethereum.request(
+          {
+            method: "eth_requestAccounts"
+          }
+        );
+        setAccount(accounts);
+      }
     }
-  }
 
+    useEffect(() => 
+    {
+      connectAccounts();
+    }, []);
+
+
+  async function mintsAnNFT() {
+
+    if (window.ethereum)
+    {
+      setLoading(true)
+      // Connect web3
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      const signer = provider.getSigner();
+      // Sign transaction, use provider to read from ethereum
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          CONTRACT_ABI,
+          signer
+      );
+ 
+      try 
+      {
+  
+          const tx =  await contract.mint();
+          const txReceipt = await tx.wait();
+          console.log("Transaction hash = ", txReceipt);
+  
+            setLoading(false)
+            alert('Yayy! minted a GoldenMan')
+      } 
+      catch (e) 
+      {
+        setLoading(false)
+        console.log(e)
+      }
+
+    }
+
+
+    }
+  
  return (
    loading ? 
   <div className="App">
